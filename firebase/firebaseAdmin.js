77a952103fb6c54092552ab05fbe47b5f8b3cc9b
Devashish Rathod <rathod.devashish09@gmail.com>
2025-10-23 22:00:00 +0530

    refactor code

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
-
+// const serviceAccount = require("../astrovani-firebase-adminsdk.json");
 const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
